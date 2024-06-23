97515c6dceb59db71462b2ef83131f1dff08ce8c
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-06-23 12:16:03 +0600

    fix: added metadata and twitter og

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     "nextjs-transferlist-reactjs",
     "next",
   ],
-  metadataBase: new URL("https://transferlist-react.vercel.app/"),
+  metadataBase: new URL("https://react-transfer-list-plum.vercel.app/"),
   alternates: {
     canonical: "/",
     languages: {
@@ -28,7 +28,23 @@ export const metadata: Metadata = {
     },
   },
   openGraph: {
-    images: "/opengraph-image.png",
+    type: "website",
+    locale: "en_US",
+    phoneNumbers: ["+8801780064264"],
+    emails: ["hasan@basikmarketing.in", "mahmudulnayeem71@gmail.com"],
+    images: [{ url: "/opengraph-image.png" }],
+    title: "React transfer list",
+    description:
+      "The Transfer List project provides a versatile and customizable solution for managing and transferring items between two lists. This interactive and user-friendly component is designed to streamline data transfer operations within your web application.",
+    siteName: "React transfer list",
+    url: "https://react-transfer-list-plum.vercel.app/",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "React transfer list",
+    images: [{ url: "/opengraph-image.png" }],
+    description:
+      "The Transfer List project provides a versatile and customizable solution for managing and transferring items between two lists. This interactive and user-friendly component is designed to streamline data transfer operations within your web application.",
   },
 };
 
